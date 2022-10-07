a99f2181d326d40b19ad964feeaa14cc6c29769b
JayDeLux <iffrig@hotmail.com>
2022-10-07 16:54:16 +0200

    Replaced les cartoon API by crypto API, don't forget to install Chrome CORS plug-in

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,63 @@
 <script>
-import Character from './lib/Character.svelte'
+  import { onMount } from "svelte";
+  import Coin from "./lib/Coin.svelte";
 
-  let characters = [];
-  let page = 7;
+  let coins = [];
 
-  async function loadCharcters() {
-    const response = await fetch("https://rickandmortyapi.com/api/character?page=" + page);
-    const data = await response.json();
-    characters = data.results;
+  let coinrakingurl = "https://api.coinranking.com/v2";
+
+  async function loadcoinprices() {
+    const response = await fetch(
+      coinrakingurl + "/search-suggestions?query=bitco",
+      {
+        method: "GET",
+        headers: {
+          "accept-language": "en",
+          "Content-type": "application/json;charset=UTF-8",
+          "x-access-token": "coinranking65312d3925c14dfa96101df961aa59da1f9acaa7c04395c7",
+        },
+      }
+    );
+
+    const string = await response.text();
+    const json = string === "" ? {} : JSON.parse(string);
+
+    if (json) {
+      coins = json.data.coins;
+    } else {
+      alert("HTTP-Error: " + response.status);
+    }
   }
 
-  loadCharcters();
 
-  function nextPage(){
-    page++;
-    loadCharcters();
-  }
+  loadcoinprices();
 
-  function previousPage(){
-    page--;
-    loadCharcters();
-  }
+  onMount(() => {
+		window.onunhandledrejection = (e) => {
+		  console.log('we got exception, but the app has crashed', e);
+			// here we should gracefully show some fallback error or previous good known state
+			// this does not work though:
+			// current = C1; 
+			
+			// todo: This is unexpected error, send error to log server
+			// only way to reload page so that users can try again until error is resolved
+			// uncomment to reload page:
+			// window.location = "/oi-oi-oi";
+		}
+	})
 
 </script>
 
-<h1>Hello World</h1>
-
-
-
+<h1>Bitcoin to FIAT converter</h1>
 
 <div class="container">
-  <div class="btns">
-    <button on:click={previousPage} disabled={page===1}>Previous</button>
+  <!-- div class="btns">
+    <button on:click={previousPage} disabled={page === 1}>Previous</button>
     <button on:click={nextPage}>Next</button>
-  
-  </div>
+  </div -->
   <div class="grid">
-{#each characters as character}
-<Character character={character}/>
-{/each}
-</div>
+    {#each coins as coin}
+      <Coin {coin} />
+    {/each}
+  </div>
 </div>
---
+++ src/lib/Coin.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let coin  
+  </script>
+  
+  <div class="coin">
+
+    <h1 class="title">{coin.name}</h1>
+    <h3>uuid: {coin.uuid}</h3>
+    <img src={coin.iconUrl} alt={coin.iconUrl}/>
+    <h3>Price: {coin.price}</h3>
+    
+</div>
